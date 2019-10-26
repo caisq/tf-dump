480c99b922290367e74f69bd928cb799a9ee4814
Shanqing Cai <cais@google.com>
2019-10-26 10:52:31 -0400

    Fix some figure layout

--- tfjs_book/relu_and_simgoid_plots.py
+++ tfjs_book/relu_and_simgoid_plots.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 plt.figure(figsize=[10, 3.5])
 
-ax = plt.subplot(1, 2, 1)
+ax = plt.subplot(position=[0.1, 0.15, 0.35, 0.7])
 x = np.arange(-10, 10, 0.1)
 y = tf.nn.sigmoid(x)
 plt.plot(x, y)
@@ -15,15 +15,15 @@ plt.grid(True)
 ax.axhline(y=0, color='k')
 ax.axvline(x=0, color='k')
 
-ax = plt.subplot(1, 2, 2)
+ax = plt.subplot(position=[0.55, 0.15, 0.35, 0.7])
 y = tf.nn.relu(x)
-plt.plot(x, y)
 plt.xlim([-10, 10])
 plt.xlabel('x')
 plt.title('relu(x)')
 plt.grid(True)
 ax.axhline(y=0, color='k')
 ax.axvline(x=0, color='k')
+plt.plot(x, y)
 
 plt.savefig('Figure_3_2.png', dpi=600)
 plt.show()
